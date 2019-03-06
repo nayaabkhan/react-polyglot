f5834b7e45dd9cf98c5235d3094156dcaa0b80ce
Nayaabkhan Khan <khannayaab@gmail.com>
2019-03-06 15:02:06 +0530

    Use the new context API
    Resolves #14

--- example/greeter.js
+++ example/greeter.js
@@ -5,9 +5,4 @@ const Greeter = ({ name, t }) => (
   <h3>{t('hello_name', { name })}</h3>
 );
 
-Greeter.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  t: React.PropTypes.func.isRequired,
-};
-
 export default translate()(Greeter);
---
+++ src/i18n-context.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const I18nContext = React.createContext()
+
+export default I18nContext
