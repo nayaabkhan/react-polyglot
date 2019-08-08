9210c448b07c5810b97a162e297dc100c3959036
Reyad Attiyat <reyad@neovation.com>
2019-08-08 14:32:34 -0500

    Add new props for polyglot options
     This commits adds new props for the following polyglot options:
       - allowMissing
       - onMissingKey
       - interpolation
       - pluralRules
    
     It also removes the use of componentWillReceiveProps() since it has
     been deprecated and replaces it with componentDidUpdate().

--- src/i18n.d.ts
+++ src/i18n.d.ts
@@ -3,12 +3,44 @@
 
 import { ComponentType, ReactNode } from 'react'
 
-interface I18nProps {
-  children: ReactNode
+interface InterpolationOptions {
+    smart_count?: number | { length: number };
+    _?: string;
+    [interpolationKey: string]: any;
+}
+
+interface InterpolationTokenOptions {
+  prefix?: string;
+  suffix?: string;
+}
+
+interface PluralRules {
+    pluralTypes: {
+        [key: string]: (no: number) => number;
+    };
+    pluralTypeToLanguages: {
+        [key: string]: string[];
+    };
+}
+
+interface PolyglotOptions {
   /** Locale to use, e.g. `en` */
-  locale: number
+  locale: string;
   /** A dictionary of translations */
-  messages: object
+  messages: object;
+
+  /** A boolean to control whether missing keys are allowed **/
+  allowMissing?: boolean;
+  /** If allow missing is true this function will be called instead of default error handler **/
+  onMissingKey?: (key: string, options?: InterpolationOptions, locale?: string) => string;
+  /** An object to change the substituation syntax for interpolation by setting prefix and suffix **/
+  interpolation?: InterpolationTokenOptions;
+  /** https://github.com/airbnb/polyglot.js#custom-pluralization-rules */
+  pluralRules?: PluralRules;
+}
+
+interface I18nProps extends PolyglotOptions {
+  children: ReactNode;
 }
 
 /** Provider component to wrap your root application component in. */
