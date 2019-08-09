6db1f26997bf880ec83368b7e1d02001048bd9ae
Nayaabkhan Khan <khannayaab@gmail.com>
2019-08-09 10:03:48 +0530

    Add JSDoc comments and set type on package.json

--- src/i18n.d.ts
+++ src/i18n.d.ts
@@ -1,14 +1,17 @@
 // Type definitions for react-polyglot v0.4.0
 // TypeScript Version: 2.8
 
-import { ComponentType, ReactNode } from 'react';
+import { ComponentType, ReactNode } from 'react'
 
 interface I18nProps {
-  children: ReactNode;
-  locale: string;
-  messages: object;
+  children: ReactNode
+  /** Locale to use, e.g. `en` */
+  locale: number
+  /** A dictionary of translations */
+  messages: object
 }
 
-declare const I18n: ComponentType<I18nProps>;
+/** Provider component to wrap your root application component in. */
+declare const I18n: ComponentType<I18nProps>
 
-export default I18n;
+export default I18n
--- src/translate.d.ts
+++ src/translate.d.ts
@@ -1,20 +1,24 @@
 // Type definitions for react-polyglot v0.4.0
 // Typescript version: 2.8
 
-import { InterpolationOptions } from 'node-polyglot';
-import { ComponentType } from 'react';
+import { InterpolationOptions } from 'node-polyglot'
+import { ComponentType } from 'react'
 
 export type t = (
+  /** The key of the phrase to translate. */
   phrase: string,
+  /** The options accepted by `polyglot.t`. */
   options?: number | InterpolationOptions
-) => string;
+) => string
 
 export interface TranslateProps {
-  t: t;
+  /** Function to get translated phrase. */
+  t: t
 }
 
+/** Wrap your components with `translate` to get a prop `t` passed in. */
 declare const translate = () => <T extends object>(
   Component: ComponentType<T>
-) => ComponentType<T & TranslateProps>(Component);
+) => ComponentType<T & TranslateProps>(Component)
 
-export default translate;
+export default translate
